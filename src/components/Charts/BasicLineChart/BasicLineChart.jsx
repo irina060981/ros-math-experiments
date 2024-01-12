b24e30afdf3ae1c055de3418765034f427848812
Irina <irina060981@yandex.ru>
2024-01-12 16:46:23 +1000

    fix

--- src/components/Charts/BasicLineChart/BasicLineChart.jsx
+++ src/components/Charts/BasicLineChart/BasicLineChart.jsx
@@ -2,8 +2,8 @@ import * as React from 'react';
 import { LineChart } from '@mui/x-charts/LineChart';
 import { getRandomInt, range } from '@/utility/math-utitility.js'
 import { useState, useEffect } from 'react'
-import { MathComponent } from "mathjax-react";
 import { Box, Typography, Stack } from '@mui/material';
+import { MathJax, MathJaxContext } from "better-react-mathjax"
 
 export default function BasicLineChart({ xAxisMin, xAxisMax, yAxisMin, yAxisMax, shkala, 
                                          a, b,
@@ -31,53 +31,58 @@ export default function BasicLineChart({ xAxisMin, xAxisMax, yAxisMin, yAxisMax,
       //Ответ: y=\\left(x+${a}\\right)^2+${b}
     return (
       <>
-      <Typography textAlign='center' >
-          <MathComponent tex={mathLegend} display={false} />
-      </Typography>
-      <Typography textAlign='center' >
-          {textLegend}
-      </Typography>
-      <LineChart
-        xAxis={[{ data: xData,  max: xAxisMax, min: xAxisMin, tickInterval: shkala}
-      ]}
-  
-        yAxis={[
-          { id: 'linearAxis', scaleType: 'linear', max: yAxisMax, min: yAxisMin, tickInterval: shkala}
-      ]}
-  
-  
-        series={[
-          {
-            data: seriesData,
-            showMark: false, color: chartColorLocal
-          },
+      <MathJax hideUntilTypeset={"first"}
+        inline
+        dynamic
+      >
+        <Typography textAlign='center' >
+            {`\\(${mathLegend}\\)`}
+        </Typography>
+        <Typography textAlign='center' >
+            {textLegend}
+        </Typography>
+        <LineChart
+          xAxis={[{ data: xData,  max: xAxisMax, min: xAxisMin, tickInterval: shkala}
         ]}
-        width={1000}
-        height={850}
-        axisHighlight={{x: 'none', y: 'none'}}
-        sx={{
-          'g.MuiChartsAxis-directionY': {
-              transform: 'translate(50%,0)'
-          },
-          
-          'g.MuiChartsAxis-directionX': {
-              transform: 'translate(0, 50%)'
-          }
-      }}
-        disableAxisListener
-  
-          slotProps={{
-              legend: {
-                direction: 'row',
-                position: { vertical: 'top', horizontal: 'middle' },
-                padding: 0,
-              }
-          }}
-  
-          tooltip={{ trigger: 'none' }}
-  
-  
-      />
+    
+          yAxis={[
+            { id: 'linearAxis', scaleType: 'linear', max: yAxisMax, min: yAxisMin, tickInterval: shkala}
+        ]}
+    
+    
+          series={[
+            {
+              data: seriesData,
+              showMark: false, color: chartColorLocal
+            },
+          ]}
+          width={1000}
+          height={850}
+          axisHighlight={{x: 'none', y: 'none'}}
+          sx={{
+            'g.MuiChartsAxis-directionY': {
+                transform: 'translate(50%,0)'
+            },
+            
+            'g.MuiChartsAxis-directionX': {
+                transform: 'translate(0, 50%)'
+            }
+        }}
+          disableAxisListener
+    
+            slotProps={{
+                legend: {
+                  direction: 'row',
+                  position: { vertical: 'top', horizontal: 'middle' },
+                  padding: 0,
+                }
+            }}
+    
+            tooltip={{ trigger: 'none' }}
+    
+    
+        />
+      </MathJax> 
     </>
     );
 } 
